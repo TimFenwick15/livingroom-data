52a43e78aa551bf3e262489c1bf194c0750a28a7
Tim Fenwick <fent@ncode.com>
2017-07-28 13:39:28 +0100

    partially handle receiving no data

--- web/script.js
+++ web/script.js
@@ -1,30 +1,33 @@
 fetch('../data')
   .then(response => response.json())
   .then(jsonData => {
-    updateView(jsonData.temperature, jsonData.light);
+    if (jsonData.temperature && jsonData.light)
+      updateView(jsonData.temperature, jsonData.light);
   })
   .catch(() => console.log('Fetch error'));
 
 
+const intensity = 255; // how not-black should the colours be
+const greenness = '00'; // How much green to mix in, would need to scale with the other component
+
 // might not use this...
 function lightToColour(light) {
-
   return 'black';
 }
 
 function dayLightToColour() {
   //blue to black -> #0000FF -> #000000
   const d = new Date();
-  return '#0000' + Math.floor((Math.sin( Math.PI * d.getHours() / 24 ) ** 2) * 255).toString(16).padStart(2, '0');
+  return `#00${greenness}${Math.floor((Math.sin( Math.PI * d.getHours() / 24 ) ** 2) * intensity).toString(16).padStart(2, '0')}`;
 }
 
 function temperatureToColour(temperature) {
   // red -> blue: #FF0000 -> #0000FF
   // assume temp varies between 15 and 35 deg. Hopefully it's never outside this range!
   const temperatureFraction = (temperature - 15) / 20;
-  const red = Math.floor(temperatureFraction * 255).toString(16).padStart(2, '0');
-  const blue = Math.floor(( 1 - temperatureFraction ) * 255).toString(16).padStart(2, '0');
-  return `#${red}00${blue}`;
+  const red = Math.floor(temperatureFraction * intensity).toString(16).padStart(2, '0');
+  const blue = Math.floor(( 1 - temperatureFraction ) * intensity).toString(16).padStart(2, '0');
+  return `#${red}${greenness}${blue}`;
 }
 
 
@@ -37,3 +40,6 @@ function updateView(temperature, light) {
     `linear-gradient(${dayLightToColour(light)}, ${temperatureToColour(temperature)})`;
 }
 
+// Todo: will need a function that properly calculates gradient in colour.
+// Just using one rgb component between 0 -> intensity isn't the same as fading between chosen colours
+// Also, 0 will break everything
