eff914dc2edca17846bb53d2fc70a558c05c1cba
Tim Fenwick <fent@ncode.com>
2017-07-28 11:33:39 +0100

    Basic client styling

--- web/script.js
+++ web/script.js
@@ -1,1 +1,15 @@
-fetch('../data').then(response => response.json()).then(console.log)
+const data = {
+  temperature: 0,
+  light: 0
+};
+
+fetch('../data')
+  .then(response => response.json())
+  .then(jsonData => {
+    data.temperature = jsonData.temperature;
+    data.light = jsonData.light;
+    document.getElementById('temperature').innerHTML = data.temperature;
+    document.getElementById('light').innerHTML = data.light;
+  })
+  .catch(() => console.log('Fetch error'));
+
