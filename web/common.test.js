e9f09ff91e8b2da05a0a004fc57f7322a214dfb5
Tim Fenwick <fent@ncode.com>
2017-07-31 14:53:03 +0100

    Added some more tests, using locally installed mocha in package.json

--- web/common.test.js
+++ web/common.test.js
@@ -13,4 +13,22 @@ describe('dayLightToColour', function() {
   it('It should return #000000 at midnight', function() {
     assert.equal(livingroomData.dayLightToColour(0), '#000000');
   });
+  it('It should return #0000ff at midday', function() {
+    assert.equal(livingroomData.dayLightToColour(12), '#0000ff');
+  });
+  it('It should return #0000d9 at 3pm', function() {
+    assert.equal(livingroomData.dayLightToColour(15), '#0000d9');
+  });
+});
+
+describe('temperatureToColour', function() {
+  it('It should return #ff0000 at 35 degrees', function() {
+    assert.equal(livingroomData.temperatureToColour(35), '#ff0000');
+  });
+  it('It should return #0000ff at 15 degrees', function() {
+    assert.equal(livingroomData.temperatureToColour(15), '#0000ff');
+  });
+  it('It should return #7f007f at 25 degrees', function() {
+    assert.equal(livingroomData.temperatureToColour(25), '#7f007f');
+  });
 });
