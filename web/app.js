414ca79a8389982bf58f23adc1ac5ce76eb8e868
Tim Fenwick <fent@ncode.com>
2017-08-01 08:19:58 +0100

    Client now polls server every min

--- web/app.js
+++ web/app.js
@@ -1,6 +1,8 @@
 // start the app here
-livingroomData.models.getData()
-  .then(data => livingroomData.views.updateDataView(data.temperature, data.light));
+setInterval(() => {
+  livingroomData.models.getData()
+    .then(data => livingroomData.views.updateDataView(data.temperature, data.light));
 
-livingroomData.models.getRecordData()
-  .then(data => livingroomData.views.updateRecordView(data.temperature, data.light));
+  livingroomData.models.getRecordData()
+    .then(data => livingroomData.views.updateRecordView(data.temperature, data.light));
+}, 60000);
